4c277553cf1060c88d168c69d322d534ebab5ef5
Mohd Aatif Khan <114300008+aatifkhandev@users.noreply.github.com>
2024-01-30 18:03:23 +0530

    Add files via upload

---
+++ new c/9ternary.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int marks;
+    printf("enter number(1-100): ");
+    scanf("%d",&marks);
+     marks <=30 ? printf("fail \n") :printf ("pass \n");
+     return 0;
+}
+
+
+
