089621603237bc9478b7eccfea6add476140da79
Aatif <aatifkhan2001@gmail.com>
2025-02-12 09:39:31 +0530

    Added linkedList

--- 1singlyLL.cpp
+++ 1singlyLL.cpp
@@ -12,9 +12,9 @@ node(int data){
     this->next=NULL;
 }
 };
-//int main(){
-   // node*node1=new node(10);
-   // cout<<node1->data<<endl;
-    //cout<<node1->next<<endl;
+int main(){
+   node*node1=new node(10);
+   cout<<node1->data<<endl;
+    cout<<node1->next<<endl;
 
-//}
+}
